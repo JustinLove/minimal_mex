5f0ef71ce7efcec6911ca1cb95970b3d595b21ec
Justin Love <git@JustinLove.name>
2015-01-23 02:25:10 -0600

    Revert "Support Server Mod Help Chat"
    This reverts commit 0d7847e015e7b2c5b663b31c04aef008fb293ac1.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,6 @@ module.exports = function(grunt) {
               'LICENSE.txt',
               'README.md',
               'CHANGELOG.md',
-              'com.wondible.pa.minimal_mex/**',
               'ui/**',
               'pa/**'],
             dest: modPath,
